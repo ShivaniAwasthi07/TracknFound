432221c542b4db381ba576f2a4ffce2e740400fb
Rashi <rashiimishraa08@gmail.com>
2024-02-16 18:34:10 +0530

    developed browse_lost page

---
+++ pages/browse_lost.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from datetime import date
+def browse_lost_item_page():
+    st.title("Browse Lost Item")
+
+    category = st.selectbox("category",["Laptop","Phone","Bag","Other"])
+    date = st.date_input("Select the date")
+    lastseen = st.text_input("Last seen location")
+    description = st.text_area("Description")
+    image = st.file_uploader("Upload Image", type=["jpg", "png"])
+
+    if st.button("Submit"):
+        # Process the submitted data (e.g., save to database)
+        st.success("Item reported successfully!")
+        st.balloons()
+
+# Display the report found item page
+browse_lost_item_page()
