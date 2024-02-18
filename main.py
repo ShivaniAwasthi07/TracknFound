e6fcf156c0b2e54ec55fab9f041ebda0005740b0
Rashi <rashiimishraa08@gmail.com>
2024-02-19 01:42:20 +0530

    Fixed error in main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
 import firebase_admin
 from firebase_admin import firestore,credentials,auth,storage
-
+import streamlit as st
 from dotenv import load_dotenv
 import os
 from datetime import datetime
